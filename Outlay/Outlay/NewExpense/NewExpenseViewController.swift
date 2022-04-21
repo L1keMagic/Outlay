de73f020d4a9355993816c27d3525909d872baa0
L1keMagic <karachev.artur@gmail.com>
2022-04-21 19:01:02 +0200

    created date label

--- Outlay/Outlay/NewExpense/NewExpenseViewController.swift
+++ Outlay/Outlay/NewExpense/NewExpenseViewController.swift
@@ -11,7 +11,7 @@ class NewExpenseViewController: UIViewController {
     lazy var titleTF: UITextField = createTextField(tag: 1, placeholder: "Title")
     lazy var priceTF: UITextField = createTextField(tag: 2, placeholder: "Price")
     lazy var categoryTF: UITextField = createTextField(tag: 3, placeholder: "Category")
-    lazy var creationDateTF: UITextField = createTextField(tag: 4, placeholder: "Date")
+    lazy var creationDateLabel: UILabel = createDateLabel()
     // MARK: - Add action for save new expence and back buttons
     @objc func saveNewExpence() {
         Logger.information(message: "save new expence touched")
@@ -34,7 +34,7 @@ extension NewExpenseViewController {
         view.addSubview(titleTF)
         view.addSubview(priceTF)
         view.addSubview(categoryTF)
-        view.addSubview(creationDateTF)
+        view.addSubview(creationDateLabel)
     }
     // MARK: - Configure Actions
     fileprivate func configureActions() {
@@ -67,7 +67,7 @@ extension NewExpenseViewController {
             $0.height.equalTo(35)
             $0.width.equalToSuperview().inset(10)
         }
-        creationDateTF.snp.makeConstraints {
+        creationDateLabel.snp.makeConstraints {
             $0.top.equalTo(categoryTF.snp.bottom).inset(-10)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(35)
@@ -77,15 +77,21 @@ extension NewExpenseViewController {
     // MARK: - Create TextField
     private func createTextField(tag: Int, placeholder: String) -> UITextField {
         let textField = UITextField()
-        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.backgroundColor = Constants.backgroundBlueColor
         textField.layer.cornerRadius = 5
         textField.textColor = .black
-        textField.autocorrectionType = UITextAutocorrectionType.no
         textField.tag = tag
         textField.delegate = self
         textField.placeholder = placeholder
         return textField
+    }
+    // MARK: - Create Label
+    private func createDateLabel() -> UILabel {
+        let label = UILabel()
+        let dateManager = DateManager()
+        label.text = dateManager.getCurrentDate()
+        label.backgroundColor = Constants.backgroundBlueColor
+        return label
     }
 }
 
