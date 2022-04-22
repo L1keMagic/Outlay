4488e01d7b05d882eab8636f871a2c5d49e9fefb
L1keMagic <karachev.artur@gmail.com>
2022-04-22 15:12:39 +0200

    added formatted and unformatted date

--- Outlay/Outlay/Extentions/DateManager.swift
+++ Outlay/Outlay/Extentions/DateManager.swift
@@ -3,7 +3,21 @@ import Foundation
 class DateManager {
     func getCurrentDate() -> String {
         let formatter = DateFormatter()
-        formatter.dateFormat = "d.MM.yyyy"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter.string(from: Date())
+    }
+    func getFormattedDate(_ date: String) -> String {
+        return date.toDateString(inputDateFormat: "yyyy-MM-dd'T'HH:mm:ssZ", ouputDateFormat: "d.MM.yyyy")
+    }
+}
+
+extension String {
+    func toDateString( inputDateFormat inputFormat: String,
+                       ouputDateFormat outputFormat: String ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = inputFormat
+        let date = dateFormatter.date(from: self)
+        dateFormatter.dateFormat = outputFormat
+        return dateFormatter.string(from: date!)
     }
 }
