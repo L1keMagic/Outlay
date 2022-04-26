70ece57dd1b34224db77b4b9188e18d565d1a2fb
L1keMagic <karachev.artur@gmail.com>
2022-04-26 19:04:31 +0200

    added get categoryById

--- Outlay/Outlay/NetworkLayer/Data/CategoryRepository.swift
+++ Outlay/Outlay/NetworkLayer/Data/CategoryRepository.swift
@@ -6,6 +6,9 @@ class CategoryRepository {
     func getCategories() -> Categories {
         return categories
     }
+    func getCategoryById(id: String) -> Category? {
+        return categories.first(where: { $0.id == id })
+    }
     func insertCategory(category: Category) {
         categories.append(category)
     }
