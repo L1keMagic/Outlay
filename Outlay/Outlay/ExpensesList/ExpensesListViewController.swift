229f9ca9abf1e1e347d8bb8363ed9f04a954d046
Vlad Polozhay <vlados988111@gmail.com>
2022-05-23 21:39:59 +0300

    grouped expenses by date but unsorted yet

--- Outlay/Outlay/ExpensesList/ExpensesListViewController.swift
+++ Outlay/Outlay/ExpensesList/ExpensesListViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class ExpensesListViewController: UIViewController {
     private var tableView = UITableView()
     private var expenses: Expenses
+    private var groupedExpenses = [Expenses]()
     init(expenses: Expenses) {
         self.expenses = expenses
         super.init(nibName: nil, bundle: nil)
@@ -12,6 +13,7 @@ class ExpensesListViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        groupExpenses()
         view.addSubview(tableView)
         configureTableView()
     }
@@ -30,12 +32,12 @@ extension ExpensesListViewController: UITableViewDelegate, UITableViewDataSource
 //        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return expenses.count
+        return groupedExpenses[section].count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.expenseCellIdentifier,
                                                  for: indexPath) as? ExpenseCell
-        let expense = expenses[indexPath.row]
+        let expense = groupedExpenses[indexPath.section][indexPath.row]
         cell?.set(expense: expense)
         return cell ?? UITableViewCell()
     }
@@ -44,5 +46,37 @@ extension ExpensesListViewController: UITableViewDelegate, UITableViewDataSource
     }
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.backgroundColor = UIColor.white
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return groupedExpenses.count
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        let date = groupedExpenses[section].first?.expenseDate
+        label.text = "\(date!)"
+        label.backgroundColor = .white
+//        DOES NOT WORK HERE, NEEDS FIX
+//        label.snp.makeConstraints {
+//            $0.left.equalToSuperview().inset(5)
+//            $0.top.equalToSuperview().inset(5)
+//            $0.bottom.equalToSuperview().inset(5)
+//        }
+        return label
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+}
+extension ExpensesListViewController {
+    func groupExpenses() {
+        print("attempt to group expenses")
+        let groupedByDate = Dictionary(grouping: expenses) { (expense) -> String in
+            return expense.expenseDate
+        }
+        let keys = groupedByDate.keys.sorted(by: >)
+        keys.forEach({
+            groupedExpenses.append(groupedByDate[$0]!)
+        })
+        tableView.reloadData()
     }
 }
